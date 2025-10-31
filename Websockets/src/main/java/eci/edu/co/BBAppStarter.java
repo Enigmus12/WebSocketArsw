4cb93dd4e8a778ef3d160e84f8f9dc94eff172c5
Juan <Jdrorodriguez2@gmail.com>
2025-10-30 19:36:33 -0500

    Interactive Board

---
+++ Websockets/src/main/java/eci/edu/co/BBAppStarter.java
@@ -0,0 +1,13 @@
+package eci.edu.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BBAppStarter {
+    public static void main(String[] args){
+        SpringApplication.run(BBAppStarter.class,args);
+    }
+    
+}
+
